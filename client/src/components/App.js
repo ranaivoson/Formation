358ef32b42d70ed26ef949cb2018b73778b80f86
ranaivoson <ranfidy@hotmail.com>
2019-03-08 18:39:27 +0100

    Header bootstrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import Login from './Login';
+import Register from './Register';
+import Header from './Header';
 
 const App = () => {
     return (
-        <div>
-            Appli
+        <div className="container-fluid">
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path="/login" exact component={Login}/>
+                    <Route path="/register" exact component={Register}/>
+                </div>
+            </BrowserRouter>
         </div>
-    )
+    );
 };
 
 export default App;
